b22d3fcc25c04891c36bae4b048ca0c1e5e56c8f
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-23 01:24:57 +0530

    Added: Wolcome message to the master

---
+++ src/Line_Comparision.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Class for Line Comparission Computation Problem
+ */
+class LineComparission
+{
+    function welcomeMessage()
+    {
+        echo "Welcome to Line Comparission Computation Problem\n";
+    }
+}
+
+$lineComparission = new LineComparission();
+$lineComparission->welcomeMessage();
